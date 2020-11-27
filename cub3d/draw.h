eb4cbe73f0a61b3e7807849dbcdaa9e8d3cca4b5
KotoRina <rinaergard@yandex.ru>
2020-11-27 21:23:19 +0300

    add check '1'

--- cub3d/draw.h
+++ cub3d/draw.h
@@ -15,5 +15,6 @@ void        draw_line_anover(int x, int y, int x_end, int y_end, t_setting *set)
 void        draw_line_black(int x, int y, int x_end, int y_end, t_setting *set);
 void        draw_line_red(int x, int y, int x_end, int y_end, t_setting *set);
 void        draw_line_green(int x, int y, int x_end, int y_end, t_setting *set);
+int         check_next_move(t_setting *set, int x_next, int y_next);
 
 #endif
--- cub3d/player_move.c
+++ cub3d/player_move.c
@@ -2,29 +2,36 @@
 
 int     move(int keycode, t_setting *set)
 {
-    if (keycode == RIGHT)
+    if (keycode == RIGHT && check_next_move(set, set->player.x + 1, set->player.y) == 1)
     {
         draw_line_black(set->player.x * 5 + 5, set->player.y * 5 + 5, set->player.x * 5 + 10, set->player.y * 5 + 10, set);
         set->player.x++;
         draw_line_red(set->player.x * 5 + 5, set->player.y * 5 + 5, set->player.x * 5 + 10, set->player.y * 5 + 10, set);
     }
-    if (keycode == LEFT)
+    if (keycode == LEFT && check_next_move(set, set->player.x - 1, set->player.y) == 1)
     {
         draw_line_black(set->player.x * 5 + 5, set->player.y * 5 + 5, set->player.x * 5 + 10, set->player.y * 5 + 10, set);
         set->player.x--;
         draw_line_red(set->player.x * 5 + 5, set->player.y * 5 + 5, set->player.x * 5 + 10, set->player.y * 5 + 10, set);
     }
-    if (keycode == UP)
+    if (keycode == UP && check_next_move(set, set->player.x, set->player.y - 1) == 1)
     {
         draw_line_black(set->player.x * 5 + 5, set->player.y * 5 + 5, set->player.x * 5 + 10, set->player.y * 5 + 10, set);
         set->player.y--;
         draw_line_red(set->player.x * 5 + 5, set->player.y * 5 + 5, set->player.x * 5 + 10, set->player.y * 5 + 10, set);
     }
-    if (keycode == DOWN)
+    if (keycode == DOWN && check_next_move(set, set->player.x, set->player.y + 1) == 1)
     {
         draw_line_black(set->player.x * 5 + 5, set->player.y * 5 + 5, set->player.x * 5 + 10, set->player.y * 5 + 10, set);
         set->player.y++;
         draw_line_red(set->player.x * 5 + 5, set->player.y * 5 + 5, set->player.x * 5 + 10, set->player.y * 5 + 10, set);
     }
     return(0);
+}
+
+int     check_next_move(t_setting *set, int x_next, int y_next)
+{
+    if (set->map.map[y_next][x_next] == '1')
+        return (-1);
+    return (1);
 }
