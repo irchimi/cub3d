677b4a843409a25ac04219a2f64d9156a8b95554
KotoRina <rinaergard@yandex.ru>
2020-12-15 02:13:18 +0300

    add clean rep

---
+++ cub3d_prod/cub3d.c
@@ -0,0 +1,14 @@
+#include <mlx.h>
+#include "cub3d.h"
+#include "types.h"
+
+int main(int count, char *params[])
+{
+    t_setting *set;
+
+    if (valid_start_data(count, params) == -1)
+        return (error_start_data());
+    set = init_data(params[1]);
+    draw_game_win(set);
+    return (0);
+}
